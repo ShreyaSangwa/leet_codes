fce0cd00c95ea80a84a9fb99bf3e4e26748af232
ShreyaSangwa <shreya.mitblr2023@learner.manipal.edu>
2025-06-05 23:50:43 +0530

    Time: 7654 ms (5%), Space: 134.6 MB (5.2%) - LeetHub

---
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.py
@@ -0,0 +1,12 @@
+from itertools import permutations
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        b=[]
+        a=[subset for subset in permutations(digits,3) if (subset[2]%2==0 and subset[0]!=0)]
+        
+        for i in a:
+            c=''.join(map(str,i))
+            b.append(int(c))
+        b=list(set(b))
+        b.sort()
+        return b
