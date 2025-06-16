75f4c09cb27d61eeee1dcf62ca73f066292d2f61
ShreyaSangwa <shreya.mitblr2023@learner.manipal.edu>
2025-06-16 15:07:27 +0530

    Time: 577 ms (67.49%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+DELETE FROM Person 
+WHERE id IN (
+    SELECT p1.id 
+    FROM Person p1
+    JOIN Person p2 ON p1.email = p2.email AND p1.id > p2.id
+);
